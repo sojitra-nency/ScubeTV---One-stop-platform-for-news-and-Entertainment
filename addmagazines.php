76ef54f45d74abd759c653053adb889fc8ca1846
Nency <snency16@gmail.com>
2023-02-12 09:32:30 +0530

    commit

---
+++ addmagazines.php
@@ -0,0 +1,17 @@
+<?php
+
+include ('connect.php');
+
+$image=$_POST['image'];
+$name=$_POST['name'];
+$url=$_POST['url'];
+$price=$_POST['price'];
+
+
+$query="INSERT INTO `magazines` (`image`,`name`,`url`,`price`) VALUES ('$image','$name','$url','$price')";
+
+$run=mysqli_query($connect,$query);
+if($run)
+    header('location:adminmain.php');
+
+?>
